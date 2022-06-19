852b479bb37331f72b0335788b39ae34475d0750
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-06-19 19:22:16 -0300

    add: cors middleware¿

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import notesRouter from './notes/routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 1337;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/', notesRouter);
 
